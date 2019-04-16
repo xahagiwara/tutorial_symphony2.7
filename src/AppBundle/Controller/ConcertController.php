c455e1b45e0cb8275b21235742990e1f8ebf99ef
xahagiwara <s.hagiwara@xearts.jp>
2019-04-16 14:22:16 +0900

    add_Concert_page

---
+++ src/AppBundle/Controller/ConcertController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+
+class ConcertController extends Controller{
+    /**
+     * @Route("/concert/")
+     */
+    public function indexAction(){
+        return $this->render('Concert/index.html.twig');
+    }
+}
