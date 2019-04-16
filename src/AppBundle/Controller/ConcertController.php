c53d4196b529b05295e3e6426c067be9e2cdf869
xahagiwara <s.hagiwara@xearts.jp>
2019-04-16 14:35:43 +0900

    add_concert_concertList

--- src/AppBundle/Controller/ConcertController.php
+++ src/AppBundle/Controller/ConcertController.php
@@ -10,6 +10,23 @@ class ConcertController extends Controller{
      * @Route("/concert/")
      */
     public function indexAction(){
-        return $this->render('Concert/index.html.twig');
+        $concertList = [
+            [
+                'data' => '2015年5月3日',
+                'time' => '14:00',
+                'place' => '東京文化会館（満席）'
+            ],[
+                'data' => '2015年7月12日',
+                'time' => '14:00',
+                'place' => '鎌倉芸術館'
+            ],[
+                'data' => '2015年9月20日',
+                'time' => '15:00',
+                'place' => '横浜みなとみらいホール'
+            ]
+        ];
+
+        return $this->render('Concert/index.html.twig',
+                [ 'concertList' => $concertList]);
     }
 }
