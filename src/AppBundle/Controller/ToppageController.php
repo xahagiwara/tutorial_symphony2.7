ad9248d3292b968a18f5c8fd45c9f0dd775f2542
xahagiwara <s.hagiwara@xearts.jp>
2019-04-16 14:11:23 +0900

    add_top

--- src/AppBundle/Controller/ToppageController.php
+++ src/AppBundle/Controller/ToppageController.php
@@ -4,14 +4,13 @@ namespace AppBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 
 class ToppageController extends Controller
 {
     /**
-     * @Route("/")
+     * @Route("/home/")
      */
     public function indexAction(){
-    
+        return $this->render('Toppage/index.html.twig');
     }
 }
