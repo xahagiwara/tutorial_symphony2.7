a65e00179238ee9f0914a396e2a641302479dd26
xearts <xearts@xeartsnoMac-mini.local>
2019-04-23 19:18:13 +0900

    add_comand

--- src/AppBundle/Command/NotifyUnprocessedInquiryCommand.php
+++ src/AppBundle/Command/NotifyUnprocessedInquiryCommand.php
@@ -6,33 +6,44 @@ use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Question\Question;
 
 class NotifyUnprocessedInquiryCommand extends ContainerAwareCommand
 {
-    protected function configure()
-    {
+    protected function configure(){
         $this
             ->setName('cs:inquiry:notify-unprocessed')
-            ->setDescription('未処理お問い合わせ一覧を通知');
+            ->setDescription('未処理のお問い合わせ一覧通知')
+        ;
     }
 
     protected function execute(InputInterface $input, OutputInterface $output)
     {
-        $container = $this->getContainer();
+        /*$container = $this->getContainer();
+
         $em = $container->get('doctrine')->getManager();
         $inquiryRepository = $em->getRepository('AppBundle:Inquiry');
+
         $inquiryList = $inquiryRepository->findUnprocessed();
 
-        if (count($inquiryList) > 0)
-        {
-            $output->writeln(count($inquiryList) . '件の未処理お問い合わせがあります');
+        if(count($inquiryList) > 0) {
+            $templating = $container->get('templating');
 
-            if ($this->confirmSend($input, $output))
-            {
-                $this->sendMail($inquiryList, $output);
-            }
-        } else {
-            $output->writeln("未処理なし");
-        }
+            $message = \Swift_Message::newInstance()
+                ->setSubject('[CS] 未処理お問い合わせ通知')
+                ->setFrom('webmaster@example.com')
+                ->setTo('admin@example.com')
+                ->setBody(
+                    $templating->render(
+                        'mail/admin_unprocessedInquiryList.txt.twig',
+                        ['inquiryList' => $inquiryList]
+                    )
+                );
+            $container->get('mailer')->send($message);
+
+            $output->writeln(count($inquiryList));
+        }else{*/
+            $output->writeln("未処理無し");
+        
     }
 }
