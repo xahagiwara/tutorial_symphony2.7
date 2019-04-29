d136271849b581d2f224518a67ddd6e1fd131656
xearts <xearts@xeartsnoMac-mini.local>
2019-04-29 17:06:52 +0900

    add_extension_log

--- src/AppBundle/DependencyInjection/AppExtension.php
+++ src/AppBundle/DependencyInjection/AppExtension.php
@@ -6,6 +6,7 @@ use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
 use Symfony\Component\HttpKernel\DependencyInjection\Extension;
 use Symfony\Component\Config\FileLocator;
+use AppBundle\Entity\MemberCollection;
 
 class AppExtension extends Extension
 {
@@ -14,6 +15,21 @@ class AppExtension extends Extension
         $loader = new YamlFileLoader($container,
             new FileLocator(__DIR__ . '/../Resources/config'));
         $loader->load('services.yml');
+
+        $config = $this->processConfiguration(new Configuration(), $configs);
+        $this->buildMemberCollectionDefinition($container, $config['members']);
+    }
+
+    private function buildMemberCollectionDefinition(ContainerBuilder $container, $memberList)
+    {
+        $collectionDefinition = $container->register('app.member_collection',
+            MemberCollection::class);
+
+        foreach ($memberList as $name => $memberInfo) {
+            $collectionDefinition->addMethodCall('addMember', [
+                $name, $memberInfo['part'], $memberInfo['joinedDate']
+            ]);
+        }
     }
 
     public function getAlias()
