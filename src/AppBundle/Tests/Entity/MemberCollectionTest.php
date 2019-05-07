0787375b5a51a0415c3f0ca88280eeca5eb6bf5a
xearts <xearts@xeartsnoMac-mini.local>
2019-05-07 16:37:56 +0900

    add_test_case

---
+++ src/AppBundle/Tests/Entity/MemberCollectionTest.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace AppBundle\Entity;
+
+class MemberCollectionTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @test
+     */
+    public function 指定した属性のメンバーが追加されていること()
+    {
+
+    }
+
+}
