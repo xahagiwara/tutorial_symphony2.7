e7950ccad5f8102df5d97e38af3fdde37bd0d0e5
xearts <xearts@xeartsnoMac-mini.local>
2019-05-07 16:46:44 +0900

    add_execution_test

--- src/AppBundle/Entity/MemberCollection.php
+++ src/AppBundle/Entity/MemberCollection.php
@@ -7,7 +7,8 @@ use Doctrine\Common\Collections\ArrayCollection;
 class MemberCollection extends ArrayCollection
 {
     public function addMember($name, $part, $joinedDate){
-        $member = new Member($name, $part, $joinedDate);
+        $port = '';
+        $member = new Member($name, $port, $joinedDate);
         $this->add($member);
     }
 }
